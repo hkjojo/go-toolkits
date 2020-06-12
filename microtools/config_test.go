a6d1d66c886ac3920b4b972716d3cdef6208a0af
Lock <lockp111@protonmail.com>
2020-06-12 16:55:21 +0800

    test: config get

--- microtools/config_test.go
+++ microtools/config_test.go
@@ -1,6 +1,8 @@
 package microtools
 
-import "testing"
+import (
+	"testing"
+)
 
 type kv struct {
 	Data string
@@ -14,5 +16,26 @@ func Test_Put(t *testing.T) {
 	}, "123")
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func Test_ConfigGet(t *testing.T) {
+	type args struct {
+		x    interface{}
+		path []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ConfigGet(tt.args.x, tt.args.path...); (err != nil) != tt.wantErr {
+				t.Errorf("ConfigGet() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
