e6360f5f3026d30d65b8a1bbe7724afe66ffd3dd
sail <ymmonstar@gmail.com>
2025-02-14 15:02:34 +0800

    feat(utils): add MatchV2

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -48,6 +48,69 @@ func TestMatch(t *testing.T) {
 	}
 }
 
+func TestMatchV2(t *testing.T) {
+	type args struct {
+		value   string
+		pattern string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"1", args{"XAUUSD", "XAUUSD*"}, true},
+		{"2", args{"XAUUSD", "!XAUUSD*"}, false},
+		{"3", args{"XAUUSD", "XAU*"}, true},
+		{"4", args{"XAUUSD", "!*XAU*"}, false},
+		{"5", args{"XAUUSD", "!XAU*,*XAU*"}, false},
+		{"6", args{"demo", "!demo*,*"}, false},
+		{"7", args{"demo1", "!demo1,*"}, false},
+		{"8", args{"demo", "real,demo"}, true},
+		{"9", args{"real\\TEST-01", "demo\\*,real\\TEST*"}, true},
+		{"10", args{"real\\TEST-01", "!real\\*,real\\TEST*"}, false},
+		{"11", args{"real\\TEST-01", "real\\*01"}, true},
+		{"12", args{"XAUUSD", "!XAUUSD.a,*XAU*,!XAGUSD.a,*XAG*"}, true},
+		{"13", args{"XAUUSD.b", "!XAUUSD.a,*XAU*,!XAGUSD.a,*XAG*"}, true},
+		{"14", args{"XAGUSD.b", "!XAUUSD.a,*XAU*,!XAGUSD.a,*XAG*"}, true},
+		{"15", args{"XAUUSD.a", "!XAUUSD.a,*XAU*,!XAGUSD.a,*XAG*"}, false},
+		{"16", args{"XAGUSD.a", "!XAUUSD.a,*XAU*,!XAGUSD.a,*XAG*"}, false},
+		{"17", args{"real\\TEST-01", "!real\\*01,!real\\*02,real\\*"}, false},
+		{"18", args{"real\\TEST-01", "real\\*01,real\\*02"}, true},
+		{"19", args{"hello", "!x*"}, true},
+		{"20", args{"abc", "a*c"}, true},
+		{"21", args{"abc", "a*b*c"}, true},
+		{"22", args{"abc", "*b*"}, true},
+		{"23", args{"abc", "a*"}, true},
+		{"24", args{"abc", "*c"}, true},
+		{"25", args{"abc", "*"}, true},
+		{"26", args{"abc", "abc"}, true},
+		{"27", args{"abc", "ab**c"}, true},
+		{"28", args{"abc", "a**c"}, true},
+		{"29", args{"abc", "a***c"}, true},
+		{"30", args{"abc", "*a*b*c*"}, true},
+		{"31", args{"abc", "a*b*c*d*"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MatchV2(tt.args.value, tt.args.pattern); got != tt.want {
+				t.Errorf("Match() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkMatch2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MatchV2("XAUUSD", "!XAUUSD*,!XAU*,*XAU*")
+	}
+}
+
+func BenchmarkMatch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Match("XAUUSD", "!XAUUSD*,!XAU*,*XAU*")
+	}
+}
+
 func TestAny(t *testing.T) {
 	type args struct {
 		value   string
