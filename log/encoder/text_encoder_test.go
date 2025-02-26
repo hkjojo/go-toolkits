d3264f2feba24751ad29eae09e2ecfd120e792d7
keating <liujijian@vanelink.net>
2025-02-26 17:44:35 +0800

    feat:acts log enum

--- log/encoder/text_encoder_test.go
+++ log/encoder/text_encoder_test.go
@@ -8,17 +8,6 @@ import (
 )
 
 func TestTextEncodeEntry(t *testing.T) {
-	type bar struct {
-		Key string  `json:"key"`
-		Val float64 `json:"val"`
-	}
-
-	type foo struct {
-		A string  `json:"aee"`
-		B int     `json:"bee"`
-		C float64 `json:"cee"`
-		D []bar   `json:"dee"`
-	}
 
 	tests := []struct {
 		desc     string
@@ -33,20 +22,6 @@ func TestTextEncodeEntry(t *testing.T) {
 				"T": "2025-02-20 20:28:42.000",
 				"N": "bob",
 				"M": "m1 tick chart",
-				"so": "passes",
-				"answer": 42,
-				"common_pie": 3.14,
-				"null_value": null,
-				"array_with_null_elements": [{}, null, null, 2],
-				"such": {
-					"aee": "lol",
-					"bee": 123,
-					"cee": 0.9999,
-					"dee": [
-						{"key": "pi", "val": 3.141592653589793},
-						{"key": "tau", "val": 6.283185307179586}
-					]
-				}
 			}`,
 			ent: zapcore.Entry{
 				Level:      zapcore.InfoLevel,
@@ -56,25 +31,6 @@ func TestTextEncodeEntry(t *testing.T) {
 			},
 			fields: []zapcore.Field{
 				zap.String("System", "Monitor"),
-				zap.String("so", "passes"),
-				zap.Int("answer", 42),
-				zap.Float64("common_pie", 3.14),
-				// Cover special-cased handling of nil in AddReflect() and
-				// AppendReflect(). Note that for the latter, we explicitly test
-				// correct results for both the nil static interface{} value
-				// (`nil`), as well as the non-nil interface value with a
-				// dynamic type and nil value (`(*struct{})(nil)`).
-				zap.Reflect("null_value", nil),
-				zap.Reflect("array_with_null_elements", []interface{}{&struct{}{}, nil, (*struct{})(nil), 2}),
-				zap.Reflect("such", foo{
-					A: "lol",
-					B: 123,
-					C: 0.9999,
-					D: []bar{
-						{"pi", 3.141592653589793},
-						{"tau", 6.283185307179586},
-					},
-				}),
 			},
 		},
 	}
--- log/kratos/acts_helper.go
+++ log/kratos/acts_helper.go
@@ -38,31 +38,31 @@ func (h *ActsHelper) log(level log.Level, keyvals ...interface{}) {
 // Debugw logs a message at debug level.
 func (h *ActsHelper) Debugw(module, source, msg string) {
 	var keyvals []interface{}
-	h.log(log.LevelDebug, append(keyvals, append(h.kvs, module, source, msgkey(msg))...)...)
+	h.log(log.LevelDebug, append(keyvals, module, source, msgkey(msg))...)
 }
 
 // Infow logs a message at info level.
 func (h *ActsHelper) Infow(module, source, msg string) {
 	var keyvals []interface{}
-	h.log(log.LevelInfo, append(keyvals, append(h.kvs, module, source, msgkey(msg))...)...)
+	h.log(log.LevelInfo, append(keyvals, module, source, msgkey(msg))...)
 }
 
 // Warnw logs a message at warnf level.
 func (h *ActsHelper) Warnw(module, source, msg string) {
 	var keyvals []interface{}
-	h.log(log.LevelWarn, append(keyvals, append(h.kvs, module, source, msgkey(msg))...)...)
+	h.log(log.LevelWarn, append(keyvals, module, source, msgkey(msg))...)
 }
 
 // Errorw logs a message at error level.
 func (h *ActsHelper) Errorw(module, source, msg string) {
 	var keyvals []interface{}
-	h.log(log.LevelError, append(keyvals, append(h.kvs, module, source, msgkey(msg))...)...)
+	h.log(log.LevelError, append(keyvals, module, source, msgkey(msg))...)
 }
 
 // Fatalw logs a message at fatal level.
 func (h *ActsHelper) Fatalw(module, source, msg string) {
 	var keyvals []interface{}
-	h.log(log.LevelFatal, append(keyvals, append(h.kvs, module, source, msgkey(msg))...)...)
+	h.log(log.LevelFatal, append(keyvals, module, source, msgkey(msg))...)
 	os.Exit(1)
 }
 
