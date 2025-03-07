40b81db59e1438de23b66efce3d93e42a6df8917
sail <ymmonstar@gmail.com>
2025-03-07 14:47:10 +0800

    feat(eval): add match multi func

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -4,13 +4,44 @@ import (
 	"testing"
 )
 
-func TestEval(t *testing.T) {
+func TestMatchIE(t *testing.T) {
 	type Test struct {
 		Symbol string `structs:"symbol"`
 	}
 	e, _ := GenMatcherWithFuncs("matchIE(symbol,'XauUSD')", &Test{}, nil)
 	b, err := e.Evaluate(map[string]interface{}{
 		"symbol": "XAUUSD",
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(b)
+}
+
+func TestMatchMulti(t *testing.T) {
+	type Test struct {
+		Symbol string `structs:"symbol"`
+	}
+	e, _ := GenMatcherWithFuncs("matchMulti(symbol,'XAUUSD')", &Test{}, nil)
+	b, err := e.Evaluate(map[string]interface{}{
+		"symbol": "X,XAUUSD",
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(b)
+}
+func TestAnyMulti(t *testing.T) {
+	type Test struct {
+		Symbol string `structs:"symbol"`
+	}
+	e, _ := GenMatcherWithFuncs("anyMulti(symbol,'xauusd,XAUUSD')", &Test{}, nil)
+	b, err := e.Evaluate(map[string]interface{}{
+		"symbol": "X,XAUUSD",
 	})
 
 	if err != nil {
